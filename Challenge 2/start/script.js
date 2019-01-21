419d8c26ce22180dcdeb8e7d6a0abb965c164c2c
RaquelAM <raquelaguilar@ciencias.unam.mx>
2019-01-21 16:46:01 -0600

    answer questions challenge 2

--- Challenge 2/start/script.js
+++ Challenge 2/start/script.js
@@ -1,7 +1,9 @@
 /* CHALLANGE 2 - Const
 Answer following Qustions:
   1. Why no error is generated after the line 14?
+  Por que se sigue haciendo referencia al mismo punto (arreglo), solo se agrego una entrada mas.
   2. Why after the line 19 TypeError is generated?
+  Por que se quiere reasignar la variable arr, lo cual haría que se cambiara el puntero
 
 Change one line of code so, that error will go away.
 Don't change lines 14, 19.
